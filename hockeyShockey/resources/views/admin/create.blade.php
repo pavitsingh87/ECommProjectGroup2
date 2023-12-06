afcd8afc3fe187bcc1cde145e8a8c85e6e654df8
Dipesh Ram Shrestha <visi.dipesh931@gmail.com>
2023-12-06 13:24:50 -0600

    Created the div container and row for the admin create bladae php

---
+++ hockeyShockey/resources/views/admin/create.blade.php
@@ -0,0 +1,9 @@
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header text-center">Hockey Player Galleries</div>
+      </div>
+    </div>
+  </div>
+</div>
