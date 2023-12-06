bac8bb1233563d29042ec3c35d5aab135476d200
Dipesh Ram Shrestha <visi.dipesh931@gmail.com>
2023-12-06 14:41:37 -0600

    created the index blade php file

--- hockeyShockey/routes/web.php
+++ hockeyShockey/routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [AdminController::class, 'dashboard'])
+    ->name('admin.dashboard')->middleware('auth');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('auth','admin');
