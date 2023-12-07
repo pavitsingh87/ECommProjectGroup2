eeb5650de5c176085fadbf8a8ea3dd6fea28daf8
simron69 <simron6917@gmail.com>
2023-12-07 12:55:49 -0600

    web.php updated

--- hockeyShockey/routes/web.php
+++ hockeyShockey/routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 
 Auth::routes();
 
@@ -30,5 +29,3 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact'); 
 });
-=======
->>>>>>> 4e22781c92271458cc8ee464a5abfbc8e2e1b1ee
