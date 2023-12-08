2ad3dea6ff871133d68312850827fd0f112bda77
simron69 <simron6917@gmail.com>
2023-12-08 09:47:38 -0600

    updated images and css, and added main

---
+++ hockey_Shockey/resources/views/layouts/main.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nova+Mono&family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+   
+    @yield('style')
+
+</head>
+
+<body>
+    @include('layouts.partials.navbar')
+   
+    @yield('content')
+    @include('layouts.partials.footer')
+</body>
+
+</html>
