ab638d7ab0faa5aadcc98339e5a9bc2efae2c43d
simron69 <simron6917@gmail.com>
2023-12-08 11:20:58 -0600

    web.php updated

--- hockey_Shockey/routes/web.php
+++ hockey_Shockey/routes/web.php
@@ -20,17 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-<<<<<<< HEAD
 Route::get('/admin', [AdminController::class, 'dashboard'])
     ->name('admin.dashboard')->middleware('auth');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('auth','admin');
-=======
-
-Route::get('/admin', [AdminController::class, 'dashboard'])
-    ->name('admin.dashboard')->middleware('auth');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('auth','admin');
-
->>>>>>> c6b8a65241f946e2bcb2c4faa88e0df1afc52f74
 
 
 // routes for about page and contact page
