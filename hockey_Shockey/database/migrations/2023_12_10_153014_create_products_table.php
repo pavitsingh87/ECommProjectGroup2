9a3428f13211c52fe53a8a351cd128f98d9fa7d7
Avneet Kaur <avneet.hayer1@gmail.com>
2023-12-10 09:49:23 -0600

    created products table again

---
+++ hockey_Shockey/database/migrations/2023_12_10_153014_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->string('product_name');
+            $table->string('product_description');
+            $table->string('product_image');
+            $table->string('product_size')->nullable();
+            $table->string('price');
+            $table->string('availability_status');
+            $table->integer('pct_id');
+            $table->integer('i_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
