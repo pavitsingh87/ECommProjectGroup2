cd5c5a56b0bbb37c19dafe19fcd8a6f160499763
simron69 <simron6917@gmail.com>
2023-12-11 14:52:10 -0600

    admin partials layout created

---
+++ hockey_Shockey/resources/views/admin/index.blade.php
@@ -0,0 +1,8 @@
+<!-- resources/views/index.blade.php -->
+
+@extends('layouts.adminpanel')
+
+@section('content')
+    <!-- Your existing content here -->
+    <h1>Welcome to the Index Page!</h1>
+@endsection
--- hockey_Shockey/resources/views/layouts/adminpanel.blade.php
+++ hockey_Shockey/resources/views/layouts/adminpanel.blade.php
@@ -1,82 +1,53 @@
-<!-- resources/views/layouts/app.blade.php -->
+<!-- resources/views/layouts/main.blade.php -->
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Responsive Admin & Dashboard Template based on Bootstrap 5">
-    <meta name="author" content="AdminKit">
-    <meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
-    <link href="https://demo-basic.adminkit.io/css/app.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
-  </head>
-  <body>
+@include('layouts.partials.admin.header') <!-- Include header template -->
+
+<body>
     <div class="wrapper">
-        <nav id="sidebar" class="sidebar js-sidebar">
-            <div class="sidebar-content js-simplebar">
-                <a class='sidebar-brand' href='/'>
-                    <span class="align-middle"><img src="{{ asset('images/logo_white.png') }}" alt="Logo"></span>
-                </a>
-                <ul class="sidebar-nav">
-                    <li class="sidebar-item">
-                        <!--<a class='sidebar-link' data-bs-toggle="collapse" href="{{ route('admin.category.index') }}">-->
-                        <a class='sidebar-link' href="{{ route('admin.category.index') }}">    
-                            <i class="align-middle" data-feather="sliders"></i>
-                            <span class="align-middle">Category</span>
-                        </a>
-                        
-                    </li>
-                    <li class="sidebar-item">
-                        <!--<a class='sidebar-link' data-bs-toggle="collapse" href="{{ route('admin.category.index') }}">-->
-                        <a class='sidebar-link' href="{{ route('admin.products.index') }}">    
-                            <i class="align-middle" data-feather="sliders"></i>
-                            <span class="align-middle">Products</span>
-                        </a>
-                        
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        @include('layouts.partials.admin.sidebar') <!-- Include sidebar template -->
+
         <div class="main">
-        <nav class="navbar navbar-expand navbar-light navbar-bg">
-          <a class="sidebar-toggle js-sidebar-toggle">
-            <i class="hamburger align-self-center"></i>
-          </a>
-          <div class="navbar-collapse collapse">
-            <ul class="navbar-nav navbar-align">
-              
-             
-              <li class="nav-item dropdown">
-               
-                <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-                  <span class="text-dark">Charles Hall</span>
+            <nav class="navbar navbar-expand navbar-light navbar-bg">
+            <a class="sidebar-toggle js-sidebar-toggle">
+                    <i class="hamburger align-self-center"></i>
                 </a>
-                <div class="dropdown-menu dropdown-menu-end">
-                  <a class='dropdown-item' href='/pages-profile'>
-                    <i class="align-middle me-1" data-feather="user"></i> Profile </a>
-                  <a class="dropdown-item" href="#">
-                    <i class="align-middle me-1" data-feather="pie-chart"></i> Analytics </a>
-                  <div class="dropdown-divider"></div>
-                  <a class='dropdown-item' href='/'>
-                    <i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy </a>
-                  <a class="dropdown-item" href="#">
-                    <i class="align-middle me-1" data-feather="help-circle"></i> Help Center </a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Log out</a>
+                <div class="navbar-collapse collapse">
+                    <ul class="navbar-nav navbar-align">
+
+
+                        <li class="nav-item dropdown">
+
+                            <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#"
+                                data-bs-toggle="dropdown">
+                                <span class="text-dark">Charles Hall</span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end">
+                                <a class='dropdown-item' href='/pages-profile'>
+                                    <i class="align-middle me-1" data-feather="user"></i> Profile </a>
+                                <a class="dropdown-item" href="#">
+                                    <i class="align-middle me-1" data-feather="pie-chart"></i> Analytics </a>
+                                <div class="dropdown-divider"></div>
+                                <a class='dropdown-item' href='/'>
+                                    <i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy </a>
+                                <a class="dropdown-item" href="#">
+                                    <i class="align-middle me-1" data-feather="help-circle"></i> Help Center </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Log out</a>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
+            </nav>
             <main class="content">
-            <div class="container-fluid p-0">
-                @yield('content') <!-- Dynamic content section -->
-            </div>
+                <div class="container-fluid p-0">
+                    @yield('content') <!-- Dynamic content section -->
+                </div>
             </main>
         </div>
     </div>
     <script src="https://demo-basic.adminkit.io/js/app.js"></script>
-  </body>
+</body>
+
 </html>
