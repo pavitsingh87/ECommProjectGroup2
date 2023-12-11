037308997fb76266cb564b425ab3f11ed8cdcfc0
simron69 <simron6917@gmail.com>
2023-12-11 17:25:31 -0600

    dashboard count items added

--- hockey_Shockey/app/Http/Controllers/ProductController.php
+++ hockey_Shockey/app/Http/Controllers/ProductController.php
@@ -10,6 +10,7 @@ class ProductController extends Controller
 {
     public function index()
     {
+        
         $products = Product::all();
         return view('admin.products.index', compact('products'));
     }
--- hockey_Shockey/resources/views/admin/index.blade.php
+++ hockey_Shockey/resources/views/admin/index.blade.php
@@ -3,6 +3,36 @@
 @extends('layouts.adminpanel')
 
 @section('content')
-    <!-- Your existing content here -->
-    <h1>Welcome to the Dashboard</h1>
+    <div class="container-fluid">
+        <h1>Welcome to the Dashboard</h1>
+
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Total Users</h5>
+                        <p class="card-text">1</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Total Products</h5>
+                        <p class="card-text">2</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Total Categories</h5>
+                        <p class="card-text">3</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
 @endsection
