18e074594f89f31a10a88066fdec0ea6e87d65bc
simron69 <simron6917@gmail.com>
2023-12-11 17:14:50 -0600

    users sidebar added

--- hockey_Shockey/resources/views/layouts/partials/admin/sidebar.blade.php
+++ hockey_Shockey/resources/views/layouts/partials/admin/sidebar.blade.php
@@ -6,10 +6,10 @@
             <span class="align-middle"><img src="{{ asset('images/logo_white.png') }}" alt="Logo"></span>
         </a>
         <ul class="sidebar-nav">
-            <li class="sidebar-item">
-                <a class='sidebar-link' href="{{ route('admin.category.index') }}">
+        <li class="sidebar-item">
+                <a class='sidebar-link' href="#">
                     <i class="align-middle" data-feather="sliders"></i>
-                    <span class="align-middle">Category</span>
+                    <span class="align-middle">Users</span>
                 </a>
             </li>
             <li class="sidebar-item">
@@ -18,6 +18,13 @@
                     <span class="align-middle">Products</span>
                 </a>
             </li>
+            <li class="sidebar-item">
+                <a class='sidebar-link' href="{{ route('admin.category.index') }}">
+                    <i class="align-middle" data-feather="sliders"></i>
+                    <span class="align-middle">Category</span>
+                </a>
+            </li>
+            
         </ul>
     </div>
 </nav>
