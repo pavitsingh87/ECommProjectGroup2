eb8863069a4a176f6fa45c728be989bf7315e0e6
simron69 <simron6917@gmail.com>
2023-12-12 12:41:39 -0600

    simron - taxes show.blade created

---
+++ hockey_Shockey/resources/views/admin/taxes/show.blade.php
@@ -0,0 +1,43 @@
+<!-- resources/views/admin/taxes/show.blade.php -->
+
+@extends('layouts.adminpanel')
+
+@section('content')
+
+<div class="container">
+    <h1>{{ $tax->province }} - {{ $tax->city }}</h1>
+
+    <div>
+        <strong>Province:</strong>
+        <p>{{ $tax->province }}</p>
+    </div>
+
+    <div>
+        <strong>City:</strong>
+        <p>{{ $tax->city }}</p>
+    </div>
+
+    <div>
+        <strong>GST:</strong>
+        <p>{{ $tax->gst }}</p>
+    </div>
+
+    <div>
+        <strong>PST:</strong>
+        <p>{{ $tax->pst }}</p>
+    </div>
+
+    <div>
+        <strong>Created At:</strong>
+        <p>{{ $tax->created_at }}</p>
+    </div>
+
+    <div>
+        <strong>Updated At:</strong>
+        <p>{{ $tax->updated_at }}</p>
+    </div>
+
+    <a href="{{ route('admin.taxes.index') }}" class="btn btn-primary">Back to Tax List</a>
+</div>
+
+@endsection
