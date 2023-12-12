7d648cf7416e8c379507cfa9e8672253daecfb9a
Avneet Kaur <avneet.hayer1@gmail.com>
2023-12-12 13:45:56 -0600

    avneet- created tax controller

--- hockey_Shockey/app/Http/Controllers/TaxController.php
+++ hockey_Shockey/app/Http/Controllers/TaxController.php
@@ -1,10 +1,57 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Tax;
 
 use Illuminate\Http\Request;
 
 class TaxController extends Controller
 {
-    //
+        public function index()
+        {
+            $taxes = Tax::all();
+            return view('admin.taxes.index', compact('taxes'));
+        }
+    
+        public function create()
+        {
+            return view('admin.taxes.create');
+        }
+    
+        public function store(Request $request)
+        {
+    
+            Tax::create([
+                'amount' => $calculatedTaxAmount,
+                'order_id' => $request->input('order_id'),
+                'province_id' => $request->input('province_id'),
+            ]);
+    
+            return redirect()->route('admin.taxes.index')->with('success', 'Tax created successfully.');
+        }
+    
+        public function show(Tax $tax)
+        {
+            return view('admin.taxes.show', compact('tax'));
+        }
+    
+        public function edit(Tax $tax)
+        {
+            return view('admin.taxes.edit', compact('tax'));
+        }
+
+        public function update(Request $request, Tax $tax)
+        {
+            $tax->update([
+                'amount' => $updatedTaxAmount,
+            ]);
+    
+            return redirect()->route('admin.taxes.index')->with('success', 'Tax updated successfully.');
+        }
+    
+        public function destroy(Tax $tax)
+        {
+            $tax->delete();
+            return redirect()->route('admin.taxes.index')->with('success', 'Tax deleted successfully.');
+        }
 }
