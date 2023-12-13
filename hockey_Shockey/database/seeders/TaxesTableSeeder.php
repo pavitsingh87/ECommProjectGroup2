6b9b2f188e24f53cdd084947575b83846bb927ef
Avneet Kaur <avneet.hayer1@gmail.com>
2023-12-12 21:37:20 -0600

    avneet- updated taxes table seeder to show tax rates for all provinces

--- hockey_Shockey/database/seeders/TaxesTableSeeder.php
+++ hockey_Shockey/database/seeders/TaxesTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Tax;
+use App\Models\Province;
 
 class TaxesTableSeeder extends Seeder
 {
@@ -13,13 +14,22 @@ class TaxesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $provinces = \App\Models\Province::pluck('id')->toArray();
+        $taxesData = [
+            ['province_id' => 1, 'gst_rate' => 5.0, 'pst_rate' => 0.0],
+            ['province_id' => 2, 'gst_rate' => 5.0, 'pst_rate' => 7.0],
+            ['province_id' => 3, 'gst_rate' => 5.0, 'pst_rate' => 7.0],
+            ['province_id' => 4, 'gst_rate' => 15.0, 'pst_rate' => 0.0],
+            ['province_id' => 5, 'gst_rate' => 15.0, 'pst_rate' => 0.0],
+            ['province_id' => 6, 'gst_rate' => 5.0, 'pst_rate' => 10.0],
+            ['province_id' => 7, 'gst_rate' => 15.0, 'pst_rate' => 8.0],
+            ['province_id' => 8, 'gst_rate' => 15.0, 'pst_rate' => 10.0],
+            ['province_id' => 9, 'gst_rate' => 5.0, 'pst_rate' => 9.975],
+            ['province_id' => 10, 'gst_rate' => 5.0, 'pst_rate' => 6.0]
+        ];
 
-            foreach ($provinces as $provinceId) {
-                Tax::create([
-                    'province_id' => $provinceId,
-                ]);
-            }
+        foreach ($taxesData as $taxData) {
+            Tax::create($taxData);
         }
     }
+}
 
