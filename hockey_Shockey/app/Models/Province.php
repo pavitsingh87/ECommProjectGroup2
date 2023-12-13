37f30cc47bde3209b47814fe769520cb8e0d6252
Avneet Kaur <avneet.hayer1@gmail.com>
2023-12-12 21:20:51 -0600

    avneet- deleted unncessary fields from province table

--- hockey_Shockey/app/Models/Province.php
+++ hockey_Shockey/app/Models/Province.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Province extends Model
 {
     use HasFactory;
-    protected $fillable = ['user_id', 'total', 'province_id', 'gst_amount', 'pst_amount'];
+    protected $fillable = ['user_id', 'total', 'province_id'];
 }
--- hockey_Shockey/database/migrations/2023_12_12_172341_create_provinces_table.php
+++ hockey_Shockey/database/migrations/2023_12_12_172341_create_provinces_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('provinces', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->decimal('gst_rate', 5, 2)->default(0);
-            $table->decimal('pst_rate', 5, 2)->default(0);
             $table->timestamps();
         });
     }
