ca6882f6de873eed13d0691321f7284f3b892f75
Avneet Kaur <avneet.hayer1@gmail.com>
2023-12-13 10:50:14 -0600

    removed unncessary fillables

--- hockey_Shockey/app/Models/Tax.php
+++ hockey_Shockey/app/Models/Tax.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tax extends Model
 {
     use HasFactory;
-    protected $fillable = ['amount', 'order_id', 'province_id', 'gst_rate', 'pst_rate'];
+    protected $fillable = ['province_id', 'gst_rate', 'pst_rate'];
     public function province()
     {
         return $this->belongsTo(Province::class);
