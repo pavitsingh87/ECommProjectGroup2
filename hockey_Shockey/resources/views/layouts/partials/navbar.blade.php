2ec7644e6acbf86933857a7db50254025a616d30
Dipesh Ram Shrestha <visi.dipesh931@gmail.com>
2023-12-14 10:48:44 -0600

    Aligned the navbar as home products contact us about us

--- hockey_Shockey/resources/views/layouts/partials/navbar.blade.php
+++ hockey_Shockey/resources/views/layouts/partials/navbar.blade.php
@@ -21,13 +21,13 @@
           <a class="nav-link" href="/">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/about">About Us</a>
-        </li>
-        <li class="nav-item">
           <a class="nav-link" href="/product">Products</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="/contact">Contact Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About Us</a>
         </li>
       </ul>
       <div class="navbar-nav navbar-info">
