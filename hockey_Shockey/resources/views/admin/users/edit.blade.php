be3fcb9342e4976d47ad134857c23f82b5d51f7d
simron69 <simron6917@gmail.com>
2023-12-14 12:17:23 -0600

    resolved issues of admin header

--- hockey_Shockey/resources/views/admin/users/edit.blade.php
+++ hockey_Shockey/resources/views/admin/users/edit.blade.php
@@ -48,16 +48,9 @@
 
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" value="{{ old('email', $user->email) }}">
+            <p>{{ $user->email }}</p>
+            <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }} " >
             @error('email')
-                <div class="text-danger">{{ $message }}</div>
-            @enderror
-        </div>
-
-        <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" name="password">
-            @error('password')
                 <div class="text-danger">{{ $message }}</div>
             @enderror
         </div>
--- hockey_Shockey/resources/views/layouts/adminpanel.blade.php
+++ hockey_Shockey/resources/views/layouts/adminpanel.blade.php
@@ -16,10 +16,12 @@
                 <div class="navbar-collapse collapse">
                     <ul class="navbar-nav navbar-align">
                         <li class="nav-item dropdown">
+                        @auth
                             <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#"
                                 data-bs-toggle="dropdown">
-                                <span class="text-dark">{{$user->first_name}} {{$user->last_name}}</span>
+                                <span class="text-dark">{{auth()->user()->first_name}} {{auth()->user()->last_name}}</span>
                             </a>
+                            @endauth
                             <div class="dropdown-menu dropdown-menu-end">
                                 <a class='dropdown-item' href='/userprofile'>
                                     <i class="align-middle me-1" data-feather="user"></i> Profile
