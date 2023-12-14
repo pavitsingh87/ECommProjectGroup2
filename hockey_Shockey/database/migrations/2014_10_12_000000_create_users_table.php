e8294aaa943d59ff34a6747dc80099c6c222e5e4
simron69 <simron6917@gmail.com>
2023-12-14 09:37:04 -0600

    users table updated- name field removed

--- hockey_Shockey/database/migrations/2014_10_12_000000_create_users_table.php
+++ hockey_Shockey/database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,7 +10,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('first_name')->nullable(); ;
             $table->string('last_name')->nullable();
             $table->string('gender')->nullable();
