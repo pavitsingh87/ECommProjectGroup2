358e36a079e832d8ee0bbd90c7acf80ee5aa8ea1
Avneet Kaur <avneet.hayer1@gmail.com>
2023-12-13 22:54:59 -0600

    wishlist is working fine, styling pending

--- hockey_Shockey/resources/views/wishlist/index.blade.php
+++ hockey_Shockey/resources/views/wishlist/index.blade.php
@@ -7,9 +7,25 @@
         <h2>My Wishlist</h2>
 
         <div class="row">
-            @forelse($wishlistItems as $item) 
-                <?php dd($item->product); ?>
-
+            @forelse($wishlistItems as $item)
+                <div class="col-lg-4 col-md-6 col-sm-6 mb-4">
+                    <div class="card h-100">
+                        <img src="{{ asset('storage/' . $item->product->product_image) }}" alt="{{ $item->product->product_name }}"
+                            class="card-img-top" />
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $item->product->product_name }}</h5>
+                            <p class="card-text">{{ $item->product->description }}</p>
+                            <p class="card-text">${{ $item->product->price }}</p>
+                        </div>
+                        <div class="card-footer">
+                            <form method="post" action="{{ route('wishlist.destroy', $item->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Remove from Wishlist</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             @empty
                 <div class="col-12">
                     <p>Your wishlist is empty.</p>
