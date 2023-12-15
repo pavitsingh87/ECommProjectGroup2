5bb741df1850625ca2872dea69242117fe1951d5
AndresFMT <andresfelipejp@hotmail.com>
2023-12-15 00:38:31 -0600

    styles button style details

--- hockey_Shockey/resources/views/home.blade.php
+++ hockey_Shockey/resources/views/home.blade.php
@@ -10,7 +10,7 @@
 <section class="products py-5">
   <div class="container px-4 px-lg-5">
     <h2 class="fs-1 align-items-center justify-content-center p-3 row"><span></span>Our Products<span></span></h2>
-    <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-3 justify-content-center">
+    <div class="row gx-4 gx-lg-5 row-cols-1 row-cols-md-3 row-cols-xl-3 justify-content-center">
       @if ($products !== null)
       @foreach ($products as $product)
       <div class="col mb-5">
@@ -57,6 +57,45 @@
       @endif
 
     </div>
+  </div>
+
+  <div class="container">
+    <div class="row g-0">
+      <div class="col-lg-4">
+        <div class="icon_box icon_box_style1">
+          <div class="icon">
+            <i class="flaticon-shipped"></i>
+          </div>
+          <div class="icon_box_content">
+            <h5>Free Delivery</h5>
+            <p>If you are going to use of Lorem, you need to be sure there anything</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="icon_box icon_box_style1">
+          <div class="icon">
+            <i class="flaticon-money-back"></i>
+          </div>
+          <div class="icon_box_content">
+            <h5>30 Day Return</h5>
+            <p>If you are going to use of Lorem, you need to be sure there anything</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="icon_box icon_box_style1">
+          <div class="icon">
+            <i class="flaticon-support"></i>
+          </div>
+          <div class="icon_box_content">
+            <h5>27/4 Support</h5>
+            <p>If you are going to use of Lorem, you need to be sure there anything</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </section>
 
