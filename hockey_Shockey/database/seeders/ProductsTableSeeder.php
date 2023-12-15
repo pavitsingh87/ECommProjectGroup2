012b1118672400476af7dea8adb3502d7253545f
Dipesh Ram Shrestha <visi.dipesh931@gmail.com>
2023-12-15 11:12:26 -0600

    Added one live data to ProductsTableSeeder inside database seeders ProductsTableSeeder php file

--- hockey_Shockey/database/seeders/ProductsTableSeeder.php
+++ hockey_Shockey/database/seeders/ProductsTableSeeder.php
@@ -12,10 +12,20 @@ class ProductsTableSeeder extends Seeder
     public function run()
     {
         // Create 15 products for each category
-        for ($categoryId = 1; $categoryId <= 4; $categoryId++) {
-            Product::factory()->count(15)->create([
-                'product_category_type_id' => $categoryId,
-            ]);
-        }
+        // for ($categoryId = 1; $categoryId <= 4; $categoryId++) {
+            // Product::factory()->count(15)->create([
+            //     'product_category_type_id' => $categoryId,
+            // ]);
+             Product::create([
+            'product_name' => 'John',
+            'product_description' => 'Doe',
+            'product_image' => 'Male',
+            'product_size' => '1990-01-01',
+            'price' => 'admin@gmail.com',
+            'availability_status' => 'available',
+            'product_category_type_id' => '1',
+            
+        ]);
+        // }
     }
 }
