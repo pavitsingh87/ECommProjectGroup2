20bef9e1d285527758e1095db6087068cae515e1
Avneet Kaur <avneet.hayer1@gmail.com>
2023-12-19 10:55:45 -0600

    added form validation and updated styles in product edit page

--- hockey_Shockey/resources/views/admin/products/edit.blade.php
+++ hockey_Shockey/resources/views/admin/products/edit.blade.php
@@ -9,47 +9,68 @@
     @csrf
     @method('PUT')
 
-    <div class="form-group">
-        <label for="product_name">Product Name</label>
-        <input type="text" name="product_name" id="product_name" class="form-control" value="{{ old('product_name', $product->product_name) }}" required>
+    <div class="form-group mb-4">
+        <strong><label for="product_name" class="mb-1">Product Name</label></strong>
+        <input type="text" name="product_name" id="product_name" class="form-control" value="{{ old('product_name', $product->product_name) }}">
+        @error('product_name')
+            <p class="text-danger">{{ $message }}</p>
+        @enderror
     </div>
 
-    <div class="form-group">
-        <label for="product_description">Product Long Description</label>
-        <textarea name="product_description" id="product_description" class="form-control" required>{{ old('product_description', $product->product_description) }}</textarea>
+    <div class="form-group mb-3">
+        <strong><label for="product_description" class="mb-1">Product Long Description</label></strong>
+        <textarea name="product_description" id="product_description" class="form-control">{{ old('product_description', $product->product_description) }}</textarea>
+        @error('product_description')
+            <p class="text-danger">{{ $message }}</p>
+        @enderror
     </div>
 
-    <div class="form-group">
-        <label for="short_description">Product Short Description</label>
-        <textarea name="short_description" id="short_description" class="form-control" required>{{ old('short_description', $product->short_description) }}</textarea>
+    <div class="form-group mb-3">
+        <strong><label for="short_description" class="mb-1">Product Short Description</label></strong>
+        <textarea name="short_description" id="short_description" class="form-control">{{ old('short_description', $product->short_description) }}</textarea>
+        @error('short_description')
+            <p class="text-danger">{{ $message }}</p>
+        @enderror
     </div>
 
-    <div class="form-group">
-        <label for="product_category_type_id">Product Category</label>
-        <select name="product_category_type_id" id="product_category_type_id" class="form-control" required>
+    <div class="form-group mb-3">
+        <strong><label for="product_category_type_id" class="mb-1">Product Category</label></strong>
+        <select name="product_category_type_id" id="product_category_type_id" class="form-control">
             @foreach($productCategories as $category)
                 <option value="{{ $category->id }}" @if($category->id == $product->product_category_type_id) selected @endif>{{ $category->pct_name }}</option>
             @endforeach
         </select>
+        @error('product_category_type_id')
+            <p class="text-danger">{{ $message }}</p>
+        @enderror
     </div>
 
-    <div class="form-group">
-        <label for="product_image">Product Image</label>
+    <div class="form-group mb-3">
+        <strong><label for="product_image" class="mb-1">Product Image</label></strong>
         <input type="file" name="product_image" id="product_image" class="form-control">
         <img src="{{ asset('storage/' . $product->product_image) }}" alt="Product Image" class="mt-2" style="max-width: 200px;">
+        @error('product_image')
+            <p class="text-danger">{{ $message }}</p>
+        @enderror
     </div>
 
-    <div class="form-group">
-        <label for="price">Price</label>
-        <input type="number" name="price" id="price" class="form-control" value="{{ old('price', $product->price) }}" required>
+    <div class="form-group mb-3">
+        <strong><label for="price" class="mb-1">Price</label></strong>
+        <input type="number" name="price" id="price" class="form-control" value="{{ old('price', $product->price) }}">
+        @error('price')
+            <p class="text-danger">{{ $message }}</p>
+        @enderror
     </div>
 
-    <div class="form-group">
-        <label for="availability_status">Availability Status</label>
+    <div class="form-group mb-3">
+        <strong><label for="availability_status" class="mb-1">Availability Status</label></strong>
         <select name="availability_status" id="availability_status" class="form-control" required>
             <option value="available" {{ old('availability_status', $product->availability_status) === 'available' ? 'selected' : '' }}>Available</option>
             <option value="out_of_stock" {{ old('availability_status', $product->availability_status) === 'out_of_stock' ? 'selected' : '' }}>Out of Stock</option>
         </select>
+        @error('availability_status')
+            <p class="text-danger">{{ $message }}</p>
+        @enderror
     </div>
 
     <button type="submit" class="btn btn-primary">Update Product</button>
