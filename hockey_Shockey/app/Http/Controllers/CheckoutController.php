723bd7a450db5eddc0646bac42f544891e6b66fe
pavitsingh87 <pavitsinghmakkar@gmail.com>
2023-12-19 14:51:59 -0600

    Update CheckoutController.php

--- hockey_Shockey/app/Http/Controllers/CheckoutController.php
+++ hockey_Shockey/app/Http/Controllers/CheckoutController.php
@@ -93,6 +93,95 @@ class CheckoutController extends Controller
     {
         return Str::random(8); // Ajusta la longitud según sea necesario
     }
+    // Function to get user transactions
+    public function getUserTransactions()
+    {
+        $userId = Auth::id();
+        
+        $query = "
+            SELECT 
+                transactions.id AS trans_id,
+                transactions.ref_number,
+                transactions.result_code,
+                transactions.result_message,
+                transactions.response_code,
+                transactions.auth_code,
+                transactions.errors,
+                transactions.trans_id,
+                transactions.status AS transaction_status,
+                transactions.created_at AS transaction_created_at,
+                transactions.updated_at AS transaction_updated_at,
+                orders.id AS order_id,
+                orders.email,
+                orders.delivery_method,
+                orders.country,
+                orders.first_name,
+                orders.last_name,
+                orders.address,
+                orders.city,
+                orders.state,
+                orders.zip_code,
+                orders.payment_status,
+                orders.user_id,
+                orders.created_at AS order_created_at,
+                orders.updated_at AS order_updated_at
+            FROM transactions
+            JOIN orders ON transactions.ref_number = orders.order_id
+            GROUP BY transactions.id, orders.id
+        ";
 
+        $result = DB::select($query);
+
+        if (empty($result)) {
+            return response()->json(['error' => 'Data not found'], 404);
+        }
+
+        $formattedItems = [];
+
+        foreach ($result as $item) {
+            $query1 = "
+                SELECT 
+                    order_items.*,
+                    products.*
+                FROM transactions
+                JOIN orders ON transactions.ref_number = orders.order_id
+                LEFT JOIN order_items ON orders.id = order_items.order_id 
+                LEFT JOIN products ON products.product_id = order_items.product_id 
+                WHERE transactions.ref_number = ?
+            ";
+            $result1 = DB::select($query1, [$item->ref_number]);
+
+            if (empty($result1)) {
+                return response()->json(['error' => 'Data not found'], 404);
+            }
+
+            $productsArray = [];
+
+            foreach ($result1 as $item1) {
+                $productsArray[] = [
+                    'product_name' => $item1->product_name,
+                    'product_description' => $item1->product_description,
+                    'short_description' => $item1->short_description,
+                    'product_image' => $item1->product_image,
+                    'product_size' => $item1->product_size,
+                ];
+            }       
+            
+            $formattedItems[] = [
+                'trans_id' => $item->trans_id,
+                'ref_number' => $item->ref_number,
+                'auth_code' => $item->auth_code,
+                'transaction_status' => $item->transaction_status,
+                'created_at' => $item->transaction_created_at,
+                'updated_at' => $item->transaction_updated_at,
+                'productsArray' => $productsArray 
+            ];
+        }
+
+        $userOrders = $formattedItems;
+
+        // Render the 'ordersProfile' view with the results
+        return view('ordersProfile', compact('userOrders'));
+    }
     
 }
