794cb0a39acc0e3c4d903f8d5b10a89616a29a27
Avneet Kaur <avneet.hayer1@gmail.com>
2023-12-20 16:58:41 -0600

    added form validations in tax create form

--- hockey_Shockey/resources/views/admin/taxes/create.blade.php
+++ hockey_Shockey/resources/views/admin/taxes/create.blade.php
@@ -13,21 +13,33 @@
         <div class="form-group">
             <label for="province">Province</label>
             <input type="text" name="province" id="province" class="form-control" value="{{ old('province') }}" required>
+            @error('province')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="gst">GST</label>
             <input type="text" name="gst" id="gst" class="form-control" value="{{ old('gst') }}" required>
+            @error('gst')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="pst">PST</label>
             <input type="text" name="pst" id="pst" class="form-control" value="{{ old('pst') }}" required>
+            @error('pst')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
 
         <div class="form-group">
             <label for="hst">HST</label>
             <input type="text" name="hst" id="hst" class="form-control" value="{{ old('hst') }}" required>
+            @error('hst')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
 
         <button type="submit" class="btn btn-primary">Add Tax</button>
